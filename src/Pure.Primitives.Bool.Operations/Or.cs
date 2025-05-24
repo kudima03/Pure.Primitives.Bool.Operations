176931785615e29309b31ec336e3597a8a7f2525
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-24 21:34:43 +0300

    Added Or operation.

---
+++ src/Pure.Primitives.Bool.Operations/Or.cs
@@ -0,0 +1,38 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+
+namespace Pure.Primitives.Bool.Operations;
+
+public sealed record Or : IBool
+{
+    private readonly IEnumerable<IBool> _parameters;
+
+    public Or(params IBool[] parameters) : this(parameters.AsReadOnly()) { }
+
+    public Or(IEnumerable<IBool> parameters)
+    {
+        _parameters = parameters;
+    }
+
+    bool IBool.Value
+    {
+        get
+        {
+            if (!_parameters.Any())
+            {
+                throw new ArgumentException();
+            }
+
+            return _parameters.Any(parameter => parameter.Value);
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
