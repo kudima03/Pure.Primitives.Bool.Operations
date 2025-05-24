8e71c9440555f6eb858b4a599c42b6c10d36e639
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-24 21:39:41 +0300

    Added Not operation.

---
+++ src/Pure.Primitives.Bool.Operations/Not.cs
@@ -0,0 +1,25 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+
+namespace Pure.Primitives.Bool.Operations;
+
+public sealed record Not : IBool
+{
+    private readonly IBool _value;
+
+    public Not(IBool value)
+    {
+        _value = value;
+    }
+
+    bool IBool.Value => !_value.Value;
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
