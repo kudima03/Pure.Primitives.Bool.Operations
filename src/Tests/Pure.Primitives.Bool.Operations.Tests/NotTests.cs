aaca4699488fb99d5281cd10833aba71cae5b4d5
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-24 21:41:46 +0300

    Added tests for Not operation.

---
+++ src/Tests/Pure.Primitives.Bool.Operations.Tests/NotTests.cs
@@ -0,0 +1,32 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+
+namespace Pure.Primitives.Bool.Operations.Tests;
+
+public sealed record NotTests
+{
+    [Fact]
+    public void ProduceCorrectValueOnTrue()
+    {
+        IBool value = new Not(new True());
+        Assert.False(value.Value);
+    }
+
+    [Fact]
+    public void ProduceCorrectValueOnFalse()
+    {
+        IBool value = new Not(new False());
+        Assert.True(value.Value);
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnGetHashCode()
+    {
+        Assert.Throws<NotSupportedException>(() => new Not(new True()).GetHashCode());
+    }
+
+    [Fact]
+    public void ThrowsExceptionOnToString()
+    {
+        Assert.Throws<NotSupportedException>(() => new Not(new True()).ToString());
+    }
+}
