c12626ef5d8119d9a628147a9c859d8cb7f98b74
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-24 21:48:06 +0300

    Implemented Xor primitive.

---
+++ src/Pure.Primitives.Bool.Operations/Xor.cs
@@ -0,0 +1,38 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+
+namespace Pure.Primitives.Bool.Operations;
+
+public sealed record Xor : IBool
+{
+    private readonly IEnumerable<IBool> _parameters;
+
+    public Xor(params IBool[] parameters) : this(parameters.AsReadOnly()) { }
+
+    public Xor(IEnumerable<IBool> parameters)
+    {
+        _parameters = parameters;
+    }
+
+    bool IBool.Value
+    {
+        get
+        {
+            if (!_parameters.Any())
+            {
+                throw new ArgumentException();
+            }
+
+            return _parameters.Count(p => p.Value) % 2 == 1;
+        }
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotSupportedException();
+    }
+
+    public override string ToString()
+    {
+        throw new NotSupportedException();
+    }
+}
