fc21d3459a184bd2d48b6cf1b6b30c0112158f24
Dmitry Kurochkin <kudima03@gmail.com>
2025-05-25 12:25:30 +0300

    Added evaluation tests for And and Or.

---
+++ src/Tests/Pure.Primitives.Bool.Operations.Tests/Fakes/BoolWithEvaluationMarker.cs
@@ -0,0 +1,28 @@
+﻿using Pure.Primitives.Abstractions.Bool;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pure.Primitives.Bool.Operations.Tests.Fakes;
+public sealed record BoolWithEvaluationMarker : IBool
+{
+    private readonly bool _value;
+
+    public BoolWithEvaluationMarker(bool value)
+    {
+        _value = value;
+    }
+
+    public bool Value
+    {
+        get
+        {
+            Evaluated = true;
+            return _value;
+        }
+    }
+
+    public bool Evaluated { get; private set; }
+}
